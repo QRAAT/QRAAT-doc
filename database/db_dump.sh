a58a8de254c529d5fb06e5e3ce43f1135106978d
Christopher Patton <cjpatton@ucdavis.edu>
2014-02-04 13:26:53 -0800

    A little more work on db scripts.

--- database/db_dump.sh
+++ database/db_dump.sh
@@ -4,7 +4,7 @@ T_START=$(date --date="20140202 1725" +%s)
 T_END=$(date --date="20140202 1900" +%s)
 
 #echo $(mysql -u reader -B -e "SELECT count(*) FROM qraat.est WHERE timestamp >= $T_START AND timestamp <= $T_END")
-mysql -u reader -B -e "SELECT count(*) FROM qraat.est WHERE timestamp >= $T_START AND timestamp <= $T_END" > est.txt
+mysql -u reader -B -e "SELECT * FROM qraat.est WHERE timestamp >= $T_START AND timestamp <= $T_END" > est.txt
 mysql -u reader -B -e "SELECT * FROM qraat.sitelist" > sitelist.txt
 mysql -u reader -B -e "SELECT * FROM qraat.txlist" > txlist.txt
 mysql -u reader -B -e "SELECT * FROM qraat.Calibration_Information" > Calibration_Information.txt
--- database/db_load.sql
+++ database/db_load.sql
@@ -8,6 +8,6 @@ LOAD DATA LOCAL INFILE 'Calibration_Information.txt' INTO TABLE qraat.Calibratio
 LOAD DATA LOCAL INFILE 'GPS_Calibration_Data.txt' INTO TABLE qraat.GPS_Calibration_Data ignore 1 lines; 
 LOAD DATA LOCAL INFILE 'True_Position.txt' INTO TABLE qraat.True_Position ignore 1 lines;
 LOAD DATA LOCAL INFILE 'Steering_Vectors.txt' INTO TABLE qraat.Steering_Vectors ignore 1 lines;
-LOAD DATA LOCAL INFILE 'est_calrun.txt' INTO TABLE qraat.est ignore 1 lines; 
+LOAD DATA LOCAL INFILE 'est.txt' INTO TABLE qraat.est ignore 1 lines; 
 
 
