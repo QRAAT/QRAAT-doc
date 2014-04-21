02a66358b1fbb606dd649ffd0c62c2d996655861
Sean Riddle <swriddle@gmail.com>
2014-04-21 12:02:22 -0700

    No underscores

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -107,8 +107,8 @@ CREATE TABLE IF NOT EXISTS qraat.est (
 CREATE TABLE IF NOT EXISTS qraat.estscore (
   ID bigint(20) NOT NULL AUTO_INCREMENT,
   estid bigint(20) NOT NULL,
-  abs_score tinyint NOT NULL,
-  rel_score double not NULL
+  absscore tinyint NOT NULL,
+  relscore double not NULL
 ) ENGINE=MyISAM ;
 
 CREATE TABLE IF NOT EXISTS qraat.telemetry ( 
