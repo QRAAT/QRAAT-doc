8cd34c0e27ab8d7f7b96dbe4fe08a6adecb778e8
Christopher Patton <chrispatton@gmail.com>
2014-05-07 12:20:53 -0700

    Updated schema.

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -105,10 +105,10 @@ CREATE TABLE IF NOT EXISTS qraat.est (
 ) ENGINE=MyISAM ;
 
 CREATE TABLE IF NOT EXISTS qraat.estscore (
-  ID bigint(20) NOT NULL AUTO_INCREMENT,
   estid bigint(20) NOT NULL,
   absscore tinyint NOT NULL,
-  relscore double not NULL
+  relscore double not NULL,
+  PRIMARY KEY (estid)
 ) ENGINE=MyISAM ;
 
 CREATE TABLE IF NOT EXISTS qraat.telemetry ( 
