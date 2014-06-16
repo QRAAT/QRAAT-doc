281b4d3076fbaa6088cce3658618d9a526f08c15
Alicia Makmuri <alicia@quail-one.(none)>
2014-06-16 14:47:01 -0700

    Added 'qraat.track' to db_load.sql.

--- database/db_load.sql
+++ database/db_load.sql
@@ -14,6 +14,7 @@ LOAD DATA LOCAL INFILE 'Calibration_Information.txt' INTO TABLE qraat.Calibratio
 LOAD DATA LOCAL INFILE 'GPS_Calibration_Data.txt' INTO TABLE qraat.GPS_Calibration_Data ignore 1 lines; 
 LOAD DATA LOCAL INFILE 'True_Position.txt' INTO TABLE qraat.True_Position ignore 1 lines;
 LOAD DATA LOCAL INFILE 'Steering_Vectors.txt' INTO TABLE qraat.Steering_Vectors ignore 1 lines;
+LOAD DATA LOCAL INFILE 'track.txt' INTO TABLE qraat.track ignore 1 lines;
 -- LOAD DATA LOCAL INFILE 'est.txt' INTO TABLE qraat.est ignore 1 lines; 
 
 
