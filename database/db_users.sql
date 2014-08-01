5157fa8435ed18bed33a5c3d06451cb5e700f6e2
Marcel Losekoot <mlosekoot@ucdavis.edu>
2014-07-31 17:11:11 -0700

    Corrected db_users.sql to specify 'localhost'

--- database/db_users.sql
+++ database/db_users.sql
@@ -4,8 +4,8 @@
 CREATE USER 'reader'@'localhost' IDENTIFIED BY 'fella'; 
 CREATE USER 'writer'@'localhost' IDENTIFIED BY 'guy'; 
 
-GRANT SELECT ON qraat.* TO 'reader', 'writer'; 
-GRANT INSERT, UPDATE, DELETE ON qraat.* TO 'writer'; 
+GRANT SELECT ON qraat.* TO 'reader'@'localhost', 'writer'@'localhost'; 
+GRANT INSERT, UPDATE, DELETE ON qraat.* TO 'writer'@'localhost'; 
 
 -- To enable `mysql -u reader` to run without entering a 
 -- password, add the following to /etc/mysql/my.cnf after 
