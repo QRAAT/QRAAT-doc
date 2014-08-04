55c1297858cacfb6f5e8786a407ad0c8757b9888
Todd Borrowman <todd.a.borrowman@gmail.com>
2014-08-04 14:33:22 -0700

    comment out create user statements, now database/db_users.sql just sets up the privilages assuming the users already exist

--- database/db_users.sql
+++ database/db_users.sql
@@ -1,8 +1,8 @@
 -- DROP USER 'reader'@'localhost';
 -- DROP USER 'writer'@'localhost'; 
 
-CREATE USER 'reader'@'localhost' IDENTIFIED BY 'fella'; 
-CREATE USER 'writer'@'localhost' IDENTIFIED BY 'guy'; 
+--CREATE USER 'reader'@'localhost' IDENTIFIED BY 'fella'; 
+--CREATE USER 'writer'@'localhost' IDENTIFIED BY 'guy'; 
 
 GRANT SELECT ON qraat.* TO 'reader'@'localhost', 'writer'@'localhost'; 
 GRANT INSERT, UPDATE, DELETE ON qraat.* TO 'writer'@'localhost'; 
@@ -15,15 +15,15 @@ GRANT INSERT, UPDATE, DELETE ON qraat.* TO 'writer'@'localhost';
 
 
 --front-end database users
-CREATE DATABASE IF NOT EXISTS django;
-CREATE USER admin@'localhost' IDENTIFIED BY 'somesecurepassword';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON django.* TO admin@'localhost';
+--CREATE DATABASE IF NOT EXISTS django;
+--CREATE USER django_admin@'localhost' IDENTIFIED BY 'somesecurepassword';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER ON django.* TO django_admin@'localhost';
 
-CREATE USER web_reader@'localhost' IDENTIFIED BY 'somesecurepassword';
+--CREATE USER web_reader@'localhost' IDENTIFIED BY 'somesecurepassword';
 GRANT SELECT ON django.* TO web_reader@'localhost';
 GRANT SELECT ON qraat.* TO web_reader@'localhost';
 
-CREATE USER web_writer@'localhost' IDENTIFIED BY 'somesecurepassword';
+--CREATE USER web_writer@'localhost' IDENTIFIED BY 'somesecurepassword';
 GRANT SELECT, INSERT, UPDATE ON django.* TO web_writer@'localhost';
 GRANT SELECT ON qraat.* TO web_writer@'localhost';
 GRANT SELECT, INSERT, UPDATE ON qraat.auth_project_collaborator TO web_writer@'localhost';
