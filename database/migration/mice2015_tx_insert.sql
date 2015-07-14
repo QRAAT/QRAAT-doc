30703da6b8998ea9fbb3251a2fa894df6cdbe4f1
Todd Borrowman <todd.a.borrowman@gmail.com>
2015-07-14 16:51:51 -0700

    database entries for dev setup

---
+++ database/migration/mice2015_tx_insert.sql
@@ -0,0 +1,55 @@
+insert into project (ownerID, name, description, is_public, is_hidden) values (4, "Mice 2015", "Mice on trapping grid, summer 2015", 0, 0);
+
+-- projectID=8
+
+-- tx_makeID=3
+insert into tx (projectID, name, serial_no, tx_makeID, frequency) values (8,'','',3,164.624), (8,'','',3,164.632), (8,'','',3,164.696),(8,'','',3,164.704),(8,'','',3,164.720),(8,'','',3,164.728),(8,'','',3,164.736),(8,'','',3,164.736),(8,'','',3,164.784),(8,'','',3,164.792),(8,'','',3,164.800);
+
+-- txIDs 114-124
+
+insert into tx_parameters (txID, name, value) values
+(114, 'pulse_width', '15'),
+(114, 'pulse_rate', '16'),
+(114, 'band3', '150'),
+(114, 'band10', '900'),
+(115, 'pulse_width', '15'),
+(115, 'pulse_rate', '16'),
+(115, 'band3', '150'),
+(115, 'band10', '900'),
+(116, 'pulse_width', '15'),
+(116, 'pulse_rate', '16'),
+(116, 'band3', '150'),
+(116, 'band10', '900'),
+(117, 'pulse_width', '15'),
+(117, 'pulse_rate', '16'),
+(117, 'band3', '150'),
+(117, 'band10', '900'),
+(118, 'pulse_width', '15'),
+(118, 'pulse_rate', '16'),
+(118, 'band3', '150'),
+(118, 'band10', '900'),
+(119, 'pulse_width', '15'),
+(119, 'pulse_rate', '16'),
+(119, 'band3', '150'),
+(119, 'band10', '900'),
+(120, 'pulse_width', '15'),
+(120, 'pulse_rate', '16'),
+(120, 'band3', '150'),
+(120, 'band10', '900'),
+(121, 'pulse_width', '15'),
+(121, 'pulse_rate', '16'),
+(121, 'band3', '150'),
+(121, 'band10', '900'),
+(122, 'pulse_width', '15'),
+(122, 'pulse_rate', '16'),
+(122, 'band3', '150'),
+(122, 'band10', '900'),
+(123, 'pulse_width', '15'),
+(123, 'pulse_rate', '16'),
+(123, 'band3', '150'),
+(123, 'band10', '900'),
+(124, 'pulse_width', '15'),
+(124, 'pulse_rate', '16'),
+(124, 'band3', '150'),
+(124, 'band10', '900');
+
