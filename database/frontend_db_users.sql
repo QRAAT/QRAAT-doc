f1724b08b1b987e4768b461ebc6f1cb69bb51ffc
Marcel Losekoot <mlosekoot@ucdavis.edu>
2015-07-22 16:10:21 -0700

    Update frontend_db_users.sql
    Added the appropriate statements from db_users.sql

--- database/frontend_db_users.sql
+++ database/frontend_db_users.sql
@@ -1,1 +1,24 @@
+-- front-end database users
+CREATE DATABASE IF NOT EXISTS django;
+CREATE USER django_admin@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, INDEX, CREATE, DROP, ALTER ON django.* TO django_admin@'localhost';
 
+CREATE USER web_reader@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT ON django.* TO web_reader@'localhost';
+GRANT SELECT ON qraat.* TO web_reader@'localhost';
+
+CREATE USER web_writer@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE ON django.* TO web_writer@'localhost';
+GRANT SELECT ON qraat.* TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.auth_project_collaborator TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.auth_project_viewer TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.deployment TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.location TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.project TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.target TO web_writer@'localhost';
+-- >>>FIXME Why is this commented out?
+-- GRANT SELECT, INSERT, UPDATE ON qraat.track TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.tx TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.tx_make TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.tx_make_parameters TO web_writer@'localhost';
+GRANT SELECT, INSERT, UPDATE ON qraat.tx_parameters TO web_writer@'localhost';
